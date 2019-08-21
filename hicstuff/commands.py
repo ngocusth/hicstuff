add step in normalization to stabilize data after division. Fix test data files to be fully upper triangle. Fix normalization test to compare upper triangle matrix appropriately

hicstuff/commands.py
@@ -312,7 +312,7 @@ class View(AbstractCommand):
         -m, --max=INT                    Saturation threshold. Maximum pixel
                                          value is set to this percentile
                                          [default: 99].
-        -n, --normalize                  Should SCN normalization be performed
+        -n, --normalize                  Should ICE normalization be performed
                                          before rendering the matrix ?
         -o, --output=IMG                 Name of the image file where the view is stored.
         -r, --region=STR[;STR]           Only view a region of the contact map.
@@ -366,11 +366,7 @@ class View(AbstractCommand):
 
         # NORMALIZATION
         if self.args["--normalize"]:
-            binned_map = hcs.normalize_sparse(binned_map, norm="SCN")
-
-        # LOG VALUES
-        # if self.args["--log"]:
-        #   binned_map = binned_map.log1p()
+            binned_map = hcs.normalize_sparse(binned_map, norm="ICE")
 
         self.vmax = np.percentile(binned_map.data, self.perc_vmax)
         # ZOOM REGION
@@ -739,7 +735,7 @@ class Scalogram(AbstractCommand):
 
         if self.args["--normalize"]:
             # good_bins = np.where(hcs.get_good_bins(S, n_std=3) == 1)[0]
-            S = hcs.normalize_sparse(S, norm="SCN")
+            S = hcs.normalize_sparse(S, norm="ICE")
             S = S.tocsr()
         if self.args["--despeckle"]:
             S = hcs.despeckle_simple(S, threads=int(self.args["--threads"]))

hicstuff/hicstuff.py
@@ -703,7 +703,7 @@ def normalize_dense(M, norm="SCN", order=1, iterations=3):
     return (s + s.T) / 2
 
 
-def normalize_sparse(M, norm="ICE", order=1, iterations=3):
+def normalize_sparse(M, norm="ICE", order=1, iterations=10):
     """Applies a normalization type to a sparse matrix.
 
     Parameters
@@ -726,20 +726,20 @@ def normalize_sparse(M, norm="ICE", order=1, iterations=3):
         Normalized sparse matrix.
     """
     # Making full symmetric matrix if not symmetric already (e.g. upper triangle)
-    r = csr_matrix(M)
-    r = r.tocoo()
+    r = coo_matrix(M)
     if norm == "ICE":
         # Row and col indices of each nonzero value in matrix
         row_indices, col_indices = r.nonzero()
-        for _ in range(1, iterations):
+        for _ in range(iterations):
             # Symmetric matrix: rows and cols have identical sums
-            row_sums = sum_mat_bins(r)
+            bin_sums = sum_mat_bins(r)
+            bin_sums /= np.mean(bin_sums)
             # Divide each nonzero value by the product of the sums of
             # their respective rows and columns.
-            r.data /= row_sums[row_indices] * row_sums[col_indices]
-        row_sums = sum_mat_bins(r)
+            r.data /= bin_sums[row_indices] * bin_sums[col_indices]
+        bin_sums = sum_mat_bins(r)
         # Scale to 1
-        r.data = r.data * (1 / np.mean(row_sums))
+        r.data = r.data * (1 / np.mean(bin_sums))
 
     elif callable(norm):
         r = norm(M)

tests/test_hicstuff.py
@@ -10,7 +10,7 @@ import random
 import numpy as np
 import pytest
 import hicstuff.hicstuff as hcs
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, triu
 from inspect import signature, getmembers, isfunction
 
 SIZE_PARAMETERS = ("matrix_size", [5, 10, 20, 50, 100])
@@ -94,18 +94,18 @@ def test_bin_bp(matrix_size):
 
 @pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")
 @pytest.mark.parametrize(*SIZE_PARAMETERS)
-def test_scn(matrix_size):
-    """Test SCN normalization
+def test_norm(matrix_size):
+    """Test matrix normalization
 
     Check whether a SCN-normalized matrix has all vectors
     summing to one. Tests both the sparse and dense algorithms.
     """
     M_d, M_s = _gen_matrices(matrix_size, full_dense=True)
     N_d = hcs.normalize_dense(M_d, "SCN", iterations=50)
-    N_s = hcs.normalize_sparse(M_s, "SCN", iterations=50)
+    N_s = hcs.normalize_sparse(M_s, "ICE", iterations=50)
     assert np.isclose(N_d.sum(axis=1), np.ones(matrix_size), rtol=0.0001).all()
-    assert np.isclose(N_s.sum(axis=1), np.ones(matrix_size), rtol=0.0001).all()
-    assert np.isclose(coo_matrix(N_d).data, N_s.data, rtol=0.000001).all()
+    assert np.isclose(hcs.sum_mat_bins(N_s), np.ones(matrix_size), rtol=0.0001).all()
+    assert np.isclose(triu(coo_matrix(N_d)).data, N_s.data, rtol=0.000001).all()
 
 
 @pytest.mark.filterwarnings("ignore::PendingDeprecationWarning")