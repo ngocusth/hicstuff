0f4c4087a1f03bf7184f8c9e8a8adcc8a148ae78
cmdoret <cyril.mattheydoret@gmail.com>
2019-10-02 21:07:24 +0200

    temporarily remove pyling from travis tests to avoid version incompatibility with pytest (?)

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ FROM ubuntu:16.04
 
 
 
-LABEL Name=hicstuff Version=1.5.10
+LABEL Name=hicstuff Version=1.5.11
 
 # Install python dependencies
 COPY * ./ /app/
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ name = "hicstuff"
 
 MAJOR = 1
 MINOR = 5
-MAINTENANCE = 10
+MAINTENANCE = 11
 VERSION = "{}.{}.{}".format(MAJOR, MINOR, MAINTENANCE)
 
 LICENSE = "GPLv3"
