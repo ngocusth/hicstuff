50e1f1855741cc4e42dc5a270d952ffaa60394cc
cmdoret <cyril.mattheydoret@gmail.com>
2020-01-20 13:22:11 +0100

    drop  python 3.8 support for now (conda env does not build in travis)

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
     "Topic :: Scientific/Engineering :: Visualization",
